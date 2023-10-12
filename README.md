05f146b1f7d43832409128a218c397ccbebe61fd
陈颖航 <brainiaccc@foxmail.com>
2023-10-12 17:13:04 +0800

    The 1.0 version of the code was uploaded

---
+++ README.md
@@ -0,0 +1,111 @@
+# 时间序列仓库
+本代码仓库是一个面向深度学习研究人员的开源库，特别是深度时间序列分析。
+
+我们提供了一个整洁的代码库来评估先进的深度时间序列模型或开发自己的模型，它涵盖了五个主流任务:**long- and short-term forecasting, imputation, anomaly detection, and classification.**
+
+## 时间序列分析排行榜
+
+截至2023年2月，五种不同任务的前三名模型是:
+
+| Model<br>Ranking | Long-term<br>Forecasting                                     | Short-term<br>Forecasting                                    | Imputation                                                   | Anomaly<br>Detection                                         | Classification                                     |
+| ---------------- | ------------------------------------------------------------ | ------------------------------------------------------------ | ------------------------------------------------------------ | ------------------------------------------------------------ | -------------------------------------------------- |
+| 🥇 1st            | [TimesNet](https://arxiv.org/abs/2210.02186)                 | [TimesNet](https://arxiv.org/abs/2210.02186)                 | [TimesNet](https://arxiv.org/abs/2210.02186)                 | [TimesNet](https://arxiv.org/abs/2210.02186)                 | [TimesNet](https://arxiv.org/abs/2210.02186)       |
+| 🥈 2nd            | [DLinear](https://github.com/cure-lab/LTSF-Linear)           | [Non-stationary<br/>Transformer](https://github.com/thuml/Nonstationary_Transformers) | [Non-stationary<br/>Transformer](https://github.com/thuml/Nonstationary_Transformers) | [Non-stationary<br/>Transformer](https://github.com/thuml/Nonstationary_Transformers) | [FEDformer](https://github.com/MAZiqing/FEDformer) |
+| 🥉 3rd            | [Non-stationary<br>Transformer](https://github.com/thuml/Nonstationary_Transformers) | [FEDformer](https://github.com/MAZiqing/FEDformer)           | [Autoformer](https://github.com/thuml/Autoformer)            | [Informer](https://github.com/zhouhaoyi/Informer2020)        | [Autoformer](https://github.com/thuml/Autoformer)  |
+
+**比较这个模型的排行榜.** ☑ 意味着它们的代码已经包含在这个repositories中
+
+  - [x] **TimesNet** - TimesNet: Temporal 2D-Variation Modeling for General Time Series Analysis [[ICLR 2023]](https://openreview.net/pdf?id=ju_Uqw384Oq) [[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/TimesNet.py)
+  - [x] **DLinear** - Are Transformers Effective for Time Series Forecasting? [[AAAI 2023]](https://arxiv.org/pdf/2205.13504.pdf) [[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/DLinear.py)
+  - [x] **LightTS** - Less Is More: Fast Multivariate Time Series Forecasting with Light Sampling-oriented MLP Structures [[arXiv 2022]](https://arxiv.org/abs/2207.01186) [[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/LightTS.py)
+  - [x] **ETSformer** - ETSformer: Exponential Smoothing Transformers for Time-series Forecasting [[arXiv 2022]](https://arxiv.org/abs/2202.01381) [[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/ETSformer.py)
+  - [x] **Non-stationary Transformer** - Non-stationary Transformers: Exploring the Stationarity in Time Series Forecasting [[NeurIPS 2022]](https://openreview.net/pdf?id=ucNDIDRNjjv) [[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/Nonstationary_Transformer.py)
+  - [x] **FEDformer** - FEDformer: Frequency Enhanced Decomposed Transformer for Long-term Series Forecasting [[ICML 2022]](https://proceedings.mlr.press/v162/zhou22g.html) [[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/FEDformer.py)
+  - [x] **Pyraformer** - Pyraformer: Low-complexity Pyramidal Attention for Long-range Time Series Modeling and Forecasting [[ICLR 2022]](https://openreview.net/pdf?id=0EXmFzUn5I) [[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/Pyraformer.py)
+  - [x] **Autoformer** - Autoformer: Decomposition Transformers with Auto-Correlation for Long-Term Series Forecasting [[NeurIPS 2021]](https://openreview.net/pdf?id=I55UqU-M11y) [[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/Autoformer.py)
+  - [x] **Informer** - Informer: Beyond Efficient Transformer for Long Sequence Time-Series Forecasting [[AAAI 2021]](https://ojs.aaai.org/index.php/AAAI/article/view/17325/17132) [[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/Informer.py)
+  - [x] **Reformer** - Reformer: The Efficient Transformer [[ICLR 2020]](https://openreview.net/forum?id=rkgNKkHtvB) [[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/Reformer.py)
+  - [x] **Transformer** - Attention is All You Need [[NeurIPS 2017]](https://proceedings.neurips.cc/paper/2017/file/3f5ee243547dee91fbd053c1c4a845aa-Paper.pdf) [[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/Transformer.py)
+
+ **Newly added baselines.** 将在综合评估后将其添加到排行榜中。
+
+------
+
+  - [x] **FiLM** - FiLM: Frequency improved Legendre Memory Model for Long-term Time Series Forecasting [[NeurIPS 2022]](https://openreview.net/forum?id=zTQdHSQUQWc)[[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/FiLM.py)
+
+  - [x] **PatchTST** - A Time Series is Worth 64 Words: Long-term Forecasting with Transformers. [[ICLR 2023]](https://openreview.net/pdf?id=Jbdc0vTOcol) [[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/PatchTST.py)
+  - [x] **MICN** - MICN: Multi-scale Local and Global Context Modeling for Long-term Series Forecasting [[ICLR 2023]](https://openreview.net/pdf?id=zt53IDUR1U)[[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/MICN.py)
+  - [x] **Crossformer** - Crossformer: Transformer Utilizing Cross-Dimension Dependency for Multivariate Time Series Forecasting [[ICLR 2023]](https://openreview.net/pdf?id=vSVLM2j9eie)[[Code]](https://github.com/thuml/Time-Series-Library/blob/main/models/Crossformer.py)
+
+## 使用方法
+
+1. 
+   安装Python 3.8。为方便起见，执行以下命令。
+
+```python
+pip install -r requirements.txt
+```
+
+2. 准备数据. 你可以从以下渠道获取数据 [[Google Drive]](https://drive.google.com/drive/folders/13Cg1KYOlzM5C7K8gK8NfC-F3EYxkM3D2?usp=sharing), [[Tsinghua Cloud]](https://cloud.tsinghua.edu.cn/f/84fbc752d0e94980a610/) or [[Baidu Drive]](https://pan.baidu.com/s/1r3KhGd0Q9PJIUZdfEYoymg?pwd=i9iy). 然后将下载的数据放在文件夹下 `./data_provider/dataset`. 下面是支持的数据集的摘要。当需要自定义的数据集时，需要将
+
+<p align="center">
+<img src=".\pic\dataset.png" height = "200" alt="" align=center />
+</p>
+
+3. 在`./scripts/`文件夹下提供了所有基准测试的实验脚本。您可以将实验结果复制为以下示例:
+
+   ```python
+   # long-term forecast
+   bash ./scripts/long_term_forecast/ETT_script/TimesNet_ETTh1.sh
+   # short-term forecast
+   bash ./scripts/short_term_forecast/TimesNet_M4.sh
+   # imputation
+   bash ./scripts/imputation/ETT_script/TimesNet_ETTh1.sh
+   # anomaly detection
+   bash ./scripts/anomaly_detection/PSM/TimesNet.sh
+   # classification
+   bash ./scripts/classification/TimesNet.sh
+   ```
+
+   若为Window操作系统，您可以按以下代码执行示例：
+
+   ```python
+   cd C:\Users\user\Desktop\Time-Series
+   python -u run.py --task_name long_term_forecast  --model informer --data ETTh1
+   ```
+
+4. 开发你自己的模型。
+
+- 将模型文件添加到`./models`文件夹中。你可以按照 `./models/Transformer.py`.
+- 将新添加的模型包含在 `./exp/exp_basic.py`的 `Exp_Basic.model_dict`中
+- 在文件夹下创建相应的脚本 `./scripts`.
+
+## 联络
+如有任何疑问或建议，欢迎联络:
+
+- 原作者：Haixu Wu (wuhx23@mails.tsinghua.edu.cn)
+- 本人: Yinghang Chen ([brainiaccc@foxmail.com]())
+
+或者可以提相关的Issues，在我看见后会尽可能解答
+
+## 关于
+
+国家重点研发计划项目(2021YFB1715200)资助。
+
+这个库是基于以下代码库构建的:
+
+- Forecasting: https://github.com/thuml/Autoformer
+
+- Anomaly Detection: https://github.com/thuml/Anomaly-Transformer
+
+- Classification: https://github.com/thuml/Flowformer
+
+所有实验数据集都是公开的，可以下链接获取:
+
+- Long-term Forecasting and Imputation: https://github.com/thuml/Autoformer
+
+- Short-term Forecasting: https://github.com/ServiceNow/N-BEATS
+
+- Anomaly Detection: https://github.com/thuml/Anomaly-Transformer
+
+- Classification: https://www.timeseriesclassification.com/
---
+++ run.py
@@ -0,0 +1,178 @@
+import argparse
+import time
+import os
+import torch
+from exp.exp_long_term_forecasting import Exp_Long_Term_Forecast
+from exp.exp_imputation import Exp_Imputation
+from exp.exp_short_term_forecasting import Exp_Short_Term_Forecast
+from exp.exp_anomaly_detection import Exp_Anomaly_Detection
+from exp.exp_classification import Exp_Classification
+import random
+import numpy as np
+
+if __name__ == '__main__':
+    fix_seed = 2023
+    random.seed(fix_seed)
+    torch.manual_seed(fix_seed)
+    np.random.seed(fix_seed)
+
+    parser = argparse.ArgumentParser(description='时间序列仓库')
+
+    # 基础配置
+    parser.add_argument('--task_name', type=str, required=True, default='long_term_forecast',
+                        help='task name, options:[long_term_forecast, short_term_forecast, imputation, classification, anomaly_detection]')
+    parser.add_argument('--is_training', type=int, default=1, help='训练状态:0代表无需训练; 1代表需要训练, options: [0,1]') 
+    parser.add_argument('--model_id', type=str, default='test', help='模型id:train代表测试集;vail代表验证集;test代表测试集, options: [train,vail,test]')
+    parser.add_argument('--model', type=str, default='Informer', required=True, help='模型名字, options: [Autoformer, Transformer, TimesNet, Informer.....]')
+
+    # data loader配置
+    parser.add_argument('--data', type=str, default='ETTh1', required=True, help='数据集类型,如果为自定义数据集则输入custom')
+    parser.add_argument('--root_path', type=str, default='./data_provider/dataset/', help='数据文件根目录')
+    parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='数据文件')
+    parser.add_argument('--features', type=str, default='MS',
+                        help='预测任务, options:[M, S, MS]; M:多对多, S:单对单, MS:多对单')
+    parser.add_argument('--target', type=str, default='OT', help='S或MS中DataFrame的目标列,若为M则可随便填写')
+    parser.add_argument('--freq', type=str, default='h',
+                        help='时间戳编码的频率, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], 同样的你可以采取15min或3h这种方式表示频率')
+    parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='模型检查点位置')
+
+    # forecasting task(预测任务配置这里)
+    parser.add_argument('--seq_len', type=int, default=96, help='encoder输入序列长度')
+    parser.add_argument('--label_len', type=int, default=48, help='start token length')
+    parser.add_argument('--pred_len', type=int, default=96, help='预测长度')
+    parser.add_argument('--seasonal_patterns', type=str, default='Monthly', help='subset for M4(只针对季节性数据集)')
+    parser.add_argument('--inverse', action='store_true', help='逆归一化输出结果', default=False)
+
+    # inputation task
+    parser.add_argument('--mask_rate', type=float, default=0.25, help='mask ratio')
+
+    # anomaly detection task(异常检测任务)
+    parser.add_argument('--anomaly_ratio', type=float, default=0.25, help='prior anomaly ratio (%)')
+
+    # 模型定义
+    parser.add_argument('--top_k', type=int, default=5, help='for TimesBlock')
+    parser.add_argument('--num_kernels', type=int, default=6, help='for Inception')
+    parser.add_argument('--enc_in', type=int, default=7, help='encoder输入维度') #
+    parser.add_argument('--dec_in', type=int, default=7, help='decoder输入维度')
+    parser.add_argument('--c_out', type=int, default=1, help='输出维度')
+    parser.add_argument('--d_model', type=int, default=512, help='升维到多少维度')
+    parser.add_argument('--n_heads', type=int, default=8, help='多头注意力的头数')
+    parser.add_argument('--e_layers', type=int, default=2, help='encoder层数')
+    parser.add_argument('--d_layers', type=int, default=1, help='decoder层数')
+    parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
+    parser.add_argument('--moving_avg', type=int, default=25, help='window size of moving average')
+    parser.add_argument('--factor', type=int, default=1, help='attn factor')
+    parser.add_argument('--distil', action='store_false',
+                        help='whether to use distilling in encoder, using this argument means not using distilling',
+                        default=True)
+    parser.add_argument('--dropout', type=float, default=0.1, help='dropout率')
+    parser.add_argument('--embed', type=str, default='timeF',
+                        help='时间特征编码方式, options:[timeF, fixed, learned]')
+    parser.add_argument('--activation', type=str, default='gelu', help='激活函数选择')
+    parser.add_argument('--output_attention', action='store_true', help='是否在encoder中输出注意力')
+
+    # optimization
+    parser.add_argument('--num_workers', type=int, default=10, help='data loader的workers数量')
+    parser.add_argument('--itr', type=int, default=1, help='experiments times')
+    parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
+    parser.add_argument('--batch_size', type=int, default=32, help='训练数据的batch size')
+    parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
+    parser.add_argument('--learning_rate', type=float, default=0.000001, help='optimizer learning rate')
+    parser.add_argument('--des', type=str, default='test', help='exp description')
+    parser.add_argument('--loss', type=str, default='MSE', help='loss function')
+    parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
+    parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+
+    # GPU
+    parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
+    parser.add_argument('--gpu', type=int, default=0, help='gpu')
+    parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+    parser.add_argument('--devices', type=str, default='0,1,2,3', help='device ids of multile gpus')
+
+    # de-stationary projector params
+    parser.add_argument('--p_hidden_dims', type=int, nargs='+', default=[128, 128],
+                        help='hidden layer dimensions of projector (List)')
+    parser.add_argument('--p_hidden_layers', type=int, default=2, help='number of hidden layers in projector')
+
+
+    args = parser.parse_args()
+    args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
+
+    if args.use_gpu and args.use_multi_gpu:
+        args.devices = args.devices.replace(' ', '')
+        device_ids = args.devices.split(',')
+        args.device_ids = [int(id_) for id_ in device_ids]
+        args.gpu = args.device_ids[0]
+
+    print('Args in experiment:')
+    print(args)
+
+    if args.task_name == 'long_term_forecast':
+        Exp = Exp_Long_Term_Forecast
+    elif args.task_name == 'short_term_forecast':
+        Exp = Exp_Short_Term_Forecast
+    elif args.task_name == 'imputation':
+        Exp = Exp_Imputation
+    elif args.task_name == 'anomaly_detection':
+        Exp = Exp_Anomaly_Detection
+    elif args.task_name == 'classification':
+        Exp = Exp_Classification
+    else:
+        Exp = Exp_Long_Term_Forecast
+
+    if args.is_training:
+        for ii in range(args.itr):
+            # setting record of experiments
+            setting = '{}_{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_fc{}_eb{}_dt{}_{}_{}'.format(
+                args.task_name,
+                args.model_id,
+                args.model,
+                args.data,
+                args.features,
+                args.seq_len,
+                args.label_len,
+                args.pred_len,
+                args.d_model,
+                args.n_heads,
+                args.e_layers,
+                args.d_layers,
+                args.d_ff,
+                args.factor,
+                args.embed,
+                args.distil,
+                args.des, ii)
+
+            exp = Exp(args)  # set experiments
+            print('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
+            exp.train(setting)
+
+            print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+            exp.test(setting)
+            torch.cuda.empty_cache()
+    else:
+        ii = 0
+        setting = '{}_{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_fc{}_eb{}_dt{}_{}_{}'.format(
+            args.task_name,
+            args.model_id,
+            args.model,
+            args.data,
+            args.features,
+            args.seq_len,
+            args.label_len,
+            args.pred_len,
+            args.d_model,
+            args.n_heads,
+            args.e_layers,
+            args.d_layers,
+            args.d_ff,
+            args.factor,
+            args.embed,
+            args.distil,
+            args.des, ii)
+
+        exp = Exp(args)  # set experiments
+        print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+        exp.test(setting, test=1)
+        torch.cuda.empty_cache()
+    end_ = time.time()
+
